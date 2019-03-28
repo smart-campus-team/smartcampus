2b6ff4dda8ecc3201a3f7d83b26f72f7bd7fb71c
AIRESH BHAT <nelliairesh@gmail.com>
2019-03-28 20:21:42 +0530

    Updates

---
+++ precache-manifest.a53defc66872fda281c9ae7a6e4e4ecf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "dc5d96ab6452918e4f7c",
+    "url": "/smart-campus/static/css/main.3b0076e1.chunk.css"
+  },
+  {
+    "revision": "dc5d96ab6452918e4f7c",
+    "url": "/smart-campus/static/js/main.e0db8808.chunk.js"
+  },
+  {
+    "revision": "ba8e3d39da3363091362",
+    "url": "/smart-campus/static/js/runtime~main.e7c8a0a9.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/smart-campus/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/smart-campus/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "3913dc877b66cc683fdc04180444baa4",
+    "url": "/smart-campus/index.html"
+  }
+];
